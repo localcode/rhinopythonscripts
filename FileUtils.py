88901daf5c2c14be371d0c9eb79c630c4e311b80
Benjamin Golder <benjamin.j.golder@gmail.com>
2011-06-10 12:34:55 -0700

    added layer import function

--- FileUtils.py
+++ FileUtils.py
@@ -10,6 +10,7 @@ def exportFile(filePath,
         geomOnly=False,
         selectedOnly=False,
         ):
+    '''Export a file.'''
     opt = FileWriteOptions()
     opt.FileVersion = version
     opt.WriteGeometryOnly = geomOnly
@@ -17,13 +18,14 @@ def exportFile(filePath,
     return scriptcontext.doc.WriteFile(filePath, opt)
 
 def importFiles(filePathList):
+    '''Import a list of files'''
     opt = FileReadOptions()
     opt.ImportMode = True
     for f in filePathList:
         scriptcontext.doc.ReadFile(f, opt)
 
 def deleteAll():
-    """Deletes everything in the current Rhino scriptcontext.document. Returns nothing."""
+    """Deletes everything in the current Rhino scriptcontext.doc. Returns nothing."""
     guidList = []
     objType = Rhino.DocObjects.ObjectType.AnyObject
     objTable = scriptcontext.doc.Objects
@@ -32,4 +34,16 @@ def deleteAll():
         guidList.append(obj.Id)
     for guid in guidList:
         objTable.Delete(guid, True)
+
+def importLayers(filePaths, layerNames):
+    '''Input a list of filePaths and a list of layerNames, in order to import all the
+    files and return a list of RhinoObjects on each layer, in corresponding layers.
+    Layers that do not exist or contain no objects will return empty lists.'''
+
+    outObjs = []
+    importFiles(filePaths)
+    for ln in layerNames:
+        objs = scriptcontext.doc.Objects.FindByLayer(ln, True)
+        outObjs.append(objs)
+    return outObjs
 
